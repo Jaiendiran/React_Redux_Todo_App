478162ca6d102bbdd2a9f95a8f126a6d6572ce04
“Jaiendiran <jaiendiran2494@gmail.com>
2025-08-18 20:30:11 +0530

    Added root reducer and splice reducer files.

--- src/reducer.js
+++ src/reducer.js
@@ -1,42 +1,18 @@
-const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
-  ],
-  filters: {
-    status: 'All',
-    colors: []
-  }
-}
+import { combineReducers } from 'redux'
 
-// Use the initialState as a default value
-export default function appReducer(state = initialState, action) {
-  // The reducer normally looks at the action type field to decide what happens
-  switch (action.type) {
-     // Do something here based on the different types of actions
-    case 'todos/todoAdded': {
-      // We need to return a new state object
-      return {
-        // that has all the existing state data
-        ...state,
-        // but has a new array for the `todos` field
-        todos: [
-          // with all of the old todos
-          ...state.todos,
-          // and the new todo object
-          {
-            // Use an auto-incrementing numeric ID for this example
-            id: nextTodoId(state.todos),
-            text: action.payload,
-            completed: false
-          }
-        ]
-      }
-    }
-    default:
-      // If this reducer doesn't recognize the action type, or doesn't
-      // care about this specific action, return the existing state unchanged
-      return state
-  }
-}
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
+
+// export default function rootReducer(state = {}, action) {
+//   return {
+//     todos: todosReducer(state.todos, action),
+//     filters: filtersReducer(state.filters, action)
+//   }
+// }
+
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filters: filtersReducer
+})
+
+export default rootReducer
